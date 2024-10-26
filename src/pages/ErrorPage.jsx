20d9b2f3acc7e81e8fdd395dc4669f3477de0004
kootmagdy <kootmagdy5@gmail.com>
2024-10-26 03:09:31 +0200

    error page

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function ErrorPage() {
+  return (
+    <div className="mt-4 mb-4 d-flex gap-3 flex-column align-items-center justify-content-center">
+      <div>
+        <h1>PAGE NOT FOUND</h1>
+      </div>
+      <div>
+        <img
+          style={{ height: 380 }}
+          src="https://th.bing.com/th/id/R.fe7a9949a6d1c3ee855e0f35bedfa2ae?rik=xZNNGe3XUb6E%2bg&riu=http%3a%2f%2fwellparkbd.com%2fimg%2f404.gif&ehk=THrlL2zpHo93BrfVjxwyQtBLEygTefkWSEAYDsFT%2fjU%3d&risl=&pid=ImgRaw&r=0"
+        />
+      </div>
+    </div>
+  );
+}

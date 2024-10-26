d4f82a465347a6b3412e98d2f0752a08a274b720
kootmagdy <kootmagdy5@gmail.com>
2024-10-26 03:12:04 +0200

    products page for admin

---
+++ src/pages/ProductsAdmin.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState, useContext } from "react";
+import MyAdminCard from "../components/MyAdminCard";
+import ProductSwiper from "../components/ProductSwiper";
+import { productAPI } from "../API/productAPI";
+import { MainContext } from "../Context/MainContext";
+
+export default function ProductsAdmin() {
+  let [products, setProducts] = useState([]);
+  let {token} = useContext(MainContext);
+  let getallproducts = () => {
+    productAPI
+      .getProducts(token)
+      .then((r) => {
+        setProducts(r.data.data.products);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getallproducts();
+  }, []);
+
+  return (
+    <div>
+      <ProductSwiper />
+      <div className="mt-5 mb-5 container">
+        <div className="row">
+          {products.map((products) => {
+            return <MyAdminCard productInfo={products} key={products._id} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
